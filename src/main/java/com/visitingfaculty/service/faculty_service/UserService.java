78744de1e8082dc621494101dccb7c34abec8550
Sameer-0 <sameershaikhx123@gmail.com>
2022-11-18 12:19:21 +0530

    mail changes

--- src/main/java/com/visitingfaculty/service/faculty_service/UserService.java
+++ src/main/java/com/visitingfaculty/service/faculty_service/UserService.java
@@ -6,9 +6,6 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 import com.visitingfaculty.service.PasswordService;
 
@@ -17,51 +14,9 @@ public class UserService {
 
     public static String uploadDirectory = "/data/tomcat/webapps/vf/imagedata";
     // System.getProperty("user.dir") + "/src/main/webapp/imagedata";  
-    
- 
-
-
-    @Autowired
-    private JavaMailSender javaMailSender;
 
     @Autowired
     PasswordService passwordService;
-
-    @Value("${spring.mail.username}")
-    private String fromEmail;
-
-    public boolean sendEmail(String message, String toEmail, int subjectType) {
-
-        String subject = "Verify Your Email Adddress";
-        String subject2 = "Application Creation Confirmation";
-        String subject3 = "OTP to Reset Password";
-        try {
-
-            SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
-
-            simpleMailMessage.setTo(toEmail);
-            simpleMailMessage.setFrom(fromEmail);
-            if (subjectType == 1) {
-            simpleMailMessage.setSubject(subject);
-                
-            } else if(subjectType == 2){
-            simpleMailMessage.setSubject(subject2);
-                
-            } else {
-            simpleMailMessage.setSubject(subject3);
-
-            }
-            simpleMailMessage.setText(message);
-
-            javaMailSender.send(simpleMailMessage);
-            return true;
-
-        } catch (Exception e) {
-
-            e.printStackTrace();
-            return false;
-        }
-    }
 
     public String uploadPhotos(String base64String) {
 
